628f8a819850e2f555660ad74c2855e4158e9564
Joshua William <joshualauw1@gmail.com>
2025-05-02 23:31:30 +0700

    [NEW]: MerchantController and project lib

---
+++ Presentation/Controllers/Admin/MerchantController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurfTicket.Presentation.Controllers.Admin
+{
+    [Route("api/admin/merchant")]
+    [ApiController]
+    [Authorize]
+    public class MerchantController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ISender _sender;
+        public MerchantController(IConfiguration configuration, ISender sender)
+        {
+            _configuration = configuration;
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMerchant()
+        {
+            return Ok();
+        }
+
+    }
+}
